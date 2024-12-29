3072e21d5b199536c758ec62b8bd58941d58e082
Jason Witherell <jwitherell@shawnee.edu>
2024-12-28 19:36:29 -0500

    initial commit
    putting our starting project onto github for the first time

---
+++ include/example/person.h
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace example
+{
+	/// <summary>
+	/// This is a simple class we'll use to explore basic C++ OOP 
+	/// (Object-Oriented Programming) concepts.  It's not very useful
+	/// in and of itself.
+	/// </summary>
+	class Person
+	{
+
+	};
+}
---
+++ src/lab01_main.cpp
@@ -0,0 +1,6 @@
+#include <person.h>
+
+int main(int argc, char** argv)
+{
+	example::Person p;
+}
